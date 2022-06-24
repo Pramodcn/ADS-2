a5c84e0d7da549059563b0796d287c6ec508db64
pramod cn <60771694+Pramodcn@users.noreply.github.com>
2022-06-24 19:55:54 +0530

    Add files via upload

---
+++ DSA-2-searching-andsorting/sort-listofstring.js
@@ -0,0 +1,26 @@
+function sortarrayString(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+var arr = [
+  "superman",
+  "doctor",
+  "books",
+  "java",
+  "css",
+  "html",
+  "datastructure",
+  "javascript",
+];
+console.log("input string:");
+console.log(arr);
+console.log("output sorted:");
+console.log(sortarrayString(arr));
